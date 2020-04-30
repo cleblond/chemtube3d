eed855b4fdac65b0177ce79bcb17ce22c210d9e4
Carl LeBlond <carl.leblond@iup.edu>
2020-04-30 17:27:18 -0400

    updates

--- index.php
+++ index.php
@@ -28,9 +28,6 @@ if ( isset($_POST['assign'])) {
     return;
 }
 
-
-
-
 // Start of the output
 $OUTPUT->header();
 $OUTPUT->bodyStart();
@@ -55,7 +52,7 @@ if ($USER->instructor) {
     echo '<form method="post">';
     echo '<label for="url_id">Select a Link to Assign:</label>';
 
-    echo '<select name="url_id" id="url_id">';
+    echo '<select class="ct3d-select" name="url_id" id="url_id">';
 
     foreach ($rows as $url) {
     
@@ -71,17 +68,10 @@ if ($USER->instructor) {
 
 
 if ($assignedrow) {
- //file_get_contents($assignedrow['url']);
- 
-    //echo '<div class="ct3diframe-container">';
 
-    echo '<p><iframe src="https://xserve.ch.liv.ac.uk/~ngreeves/wordpress/'.$assignedrow['url'].'" width="100%" height="800"></iframe></p>';
 
-    echo '<p>This uses the special URL with the modified pages - no header and footer</p>';
+    echo '<p><iframe src="https://www.chemtube3d.com/wordpress/'.$assignedrow['url'].'" width="100%" height="800"></iframe></p>';
 
-    //echo '</div>';
-     
- 
  
 
 } else {
@@ -89,10 +79,61 @@ if ($assignedrow) {
 
 }
 
+
+/*
+//this processes Nicks html index file
+$html = file_get_contents('ct3d_index.html');
+$doc = new DOMDocument();
+$doc->loadHTML($html);
+
+$links = $doc->getElementsByTagName('a');
+$extractedLinks = array();
+
+foreach($links as $link){
+ 
+    //Get the link text.
+    $linkText = $link->nodeValue;
+    echo($linkText."<br/>");
+    
+    
+    //Get the link in the href attribute.
+    $linkHref = $link->getAttribute('href');
+    
+    $linkpieces = explode("/", $linkHref);
+    
+    $PDOX->queryDie("INSERT INTO {$p}chemtube3d_url
+        (url, description)
+        VALUES ( :URL, :DES )",
+        array(
+            ':URL' => $linkpieces[3],
+            ':DES' => trim($linkText)
+        )
+    );
+    
+     echo($linkpieces[3]."<br/>");
+    //If the link is empty, skip it and don't
+    //add it to our $extractedLinks array
+    if(strlen(trim($linkHref)) == 0){
+        continue;
+    }
+ 
+   
+ 
+    
+ 
+}
+*/
+
 $OUTPUT->footerStart();
 ?>
+<link href="js/chosen/chosen.min.css" rel="stylesheet" />
+<script src="js/chosen/chosen.jquery.js"></script>
 <script>
-// You might put some JavaScript here
+  $(document).ready(function () {
+
+        $('.ct3d-select').chosen({width: "100%", include_group_label_in_selected: true});
+
+   });
 </script>
 <?php
 $OUTPUT->footerEnd();
